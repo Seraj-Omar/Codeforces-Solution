0df8cf3bd5c563b9c7a2c1e7a09885ada983ef3c
Seraj Omar <153351518+Seraj-Omar@users.noreply.github.com>
2024-06-22 18:58:08 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,53 @@
+/*
+ بسم الله الرحمن الرحيم
+ Code by SerajOmar :)
+
+     ███████ ███████ ██████   █████      ██████
+     ██      ██      ██   ██ ██   ██       ██
+     ███████ █████   ██████  ███████       ██
+          ██ ██      ██   ██ ██   ██ ██    ██
+     ███████ ███████ ██   ██ ██   ██ ████████
+                                    ⠀    -HIM-
+    LETS GOOOOO
+ */
+#include <bits/stdc++.h>
+#define SerajOmar    ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define OneTest     solve();
+#define MultipleTests     int t;cin>>t;while(t--){solve();}
+#define ll  long long
+#define vi   vector<ll>
+#define pi  pair<ll,ll>
+#define vp  vector<pi>
+#define cy  cout<<"YES"<<endl;
+#define cn  cout<<"NO"<<endl;
+#define input(n)    for(auto& i:n)cin>>i;
+#define loop(from,to)   for(int i=from;i<to;i++)
+#define loop2(from2,to2)   for(int j=from2;j<to2;j++)
+#define all(v)  v.begin(),v.end()
+#define STFU    return;
+using namespace std;
+void solve()
+{
+    ll n;cin>>n;
+    if(n<=2)
+        cout<<0<<endl;
+    else if(n==3)
+        cout<<1<<endl;
+    else
+    {
+        if(n%2==0)
+            cout<<n/2-1<<endl;
+        else
+            cout<<n/2<<endl;
+    }
+    STFU
+}
+int main()
+{
+    SerajOmar
+    MultipleTests
+}
+/*
+  CONSISTENCY.
+  Nice To Meet U ^_^
+*/
