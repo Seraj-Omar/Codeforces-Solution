7c15ba395550696e25fa9b3890d93b21dd9c55a8
Seraj Omar <153351518+Seraj-Omar@users.noreply.github.com>
2024-06-22 18:42:24 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,49 @@
+/*
+ بسم الله الرحمن الرحيم
+ Code by SerajOmar :)
+
+     ███████ ███████ ██████   █████      ██████
+     ██      ██      ██   ██ ██   ██       ██
+     ███████ █████   ██████  ███████       ██
+          ██ ██      ██   ██ ██   ██ ██    ██
+     ███████ ███████ ██   ██ ██   ██ ████████
+                                    ⠀    -HIM-
+    LETS GOOOOO
+ */
+#include <bits/stdc++.h>
+#define SerajOmar    ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define OneTest     solve();
+#define MultipleTests     int t;cin>>t;while(t--){solve();}
+#define ll  long long
+#define vi   vector<ll>
+#define pi  pair<ll,ll>
+#define vp  vector<pi>
+#define cy  cout<<"YES"<<endl;
+#define cn  cout<<"NO"<<endl;
+#define input(n)    for(auto& i:n)cin>>i;
+#define loop(from,to)   for(int i=from;i<to;i++)
+#define loop2(from2,to2)   for(int j=from2;j<to2;j++)
+#define loop3(from3,to3)   for(int k=from3;k<to3;k++)
+#define all(v)  v.begin(),v.end()
+#define STFU    return;
+using namespace std;
+void solve()
+{
+    ll a,b,k;cin>>a>>b>>k;
+    ll answer;
+    if(k%2==0)
+        answer=a*(k/2)-b*(k/2);
+    else
+        answer=a*(k/2+1)-b*(k/2);
+    cout<<answer<<endl;
+    STFU
+}
+int main()
+{
+    SerajOmar
+    MultipleTests
+}
+/*
+  CONSISTENCY.
+  Nice To Meet U ^_^
+*/
