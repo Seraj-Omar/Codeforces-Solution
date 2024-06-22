c725cdd4b3c538c76f190e1d432c8113c9d13fb3
Seraj Omar <153351518+Seraj-Omar@users.noreply.github.com>
2024-06-22 18:36:53 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,47 @@
+/*
+ بسم الله الرحمن الرحيم
+ Code by SerajOmar :)
+
+     ███████ ███████ ██████   █████      ██████
+     ██      ██      ██   ██ ██   ██       ██
+     ███████ █████   ██████  ███████       ██
+          ██ ██      ██   ██ ██   ██ ██    ██
+     ███████ ███████ ██   ██ ██   ██ ████████
+                                    ⠀    -HIM-
+    LETS GOOOOO
+ */
+#include <bits/stdc++.h>
+#define SerajOmar    ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define OneTest     solve();
+#define MultipleTests     int t;cin>>t;while(t--){solve();}
+#define ll  long long
+#define vi   vector<ll>
+#define pi  pair<ll,ll>
+#define vp  vector<pi>
+#define cy  cout<<"YES"<<endl;
+#define cn  cout<<"NO"<<endl;
+#define input(n)    for(auto& i:n)cin>>i;
+#define loop(from,to)   for(int i=from;i<to;i++)
+#define loop2(from2,to2)   for(int j=from2;j<to2;j++)
+#define loop3(from3,to3)   for(int k=from3;k<to3;k++)
+#define all(v)  v.begin(),v.end()
+#define STFU    return;
+using namespace std;
+void solve()
+{
+    ll n;cin>>n;
+    vi v(n-1);
+    input(v)
+    ll last;cin>>last;
+    cout<< *std::max_element(v.begin(), v.end())+last<<endl;
+    STFU
+}
+int main()
+{
+    SerajOmar
+    MultipleTests
+}
+/*
+  CONSISTENCY.
+  Nice To Meet U ^_^
+*/
