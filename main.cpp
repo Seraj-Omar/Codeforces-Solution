cf83376e30ba0da04fada2baa6dd6a2d3eef7cee
Seraj Omar <153351518+Seraj-Omar@users.noreply.github.com>
2024-06-22 21:06:45 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,51 @@
+/*
+ بسم الله الرحمن الرحيم
+
+ Code by SerajOmar :)
+ Accepted	46 ms	0 KB
+
+     ███████ ███████ ██████   █████      ██████
+     ██      ██      ██   ██ ██   ██       ██
+     ███████ █████   ██████  ███████       ██
+          ██ ██      ██   ██ ██   ██ ██    ██
+     ███████ ███████ ██   ██ ██   ██ ████████
+                                    ⠀    -HIM-
+    LETS GOOOOO
+ */
+#include <bits/stdc++.h>
+#define SerajOmar    ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define OneTest     solve();
+#define MultipleTests     int t;cin>>t;while(t--){solve();}
+#define ll  long long
+#define vi   vector<ll>
+#define pi  pair<ll,ll>
+#define vp  vector<pi>
+#define cy  cout<<"YES"<<endl;
+#define cn  cout<<"NO"<<endl;
+#define input(n)    for(auto& i:n)cin>>i;
+#define loop(from,to)   for(int i=from;i<to;i++)
+#define loop2(from2,to2)   for(int j=from2;j<to2;j++)
+#define all(v)  v.begin(),v.end()
+#define STFU    return;
+using namespace std;
+void solve()
+{
+    char x;cin>>x;
+    string s="codeforces";
+
+    if(s.find(x)!=s.npos)
+        cy
+    else
+        cn
+
+    STFU
+}
+int main()
+{
+    SerajOmar
+    MultipleTests
+}
+/*
+  CONSISTENCY.
+  Nice To Meet U ^_^
+*/
