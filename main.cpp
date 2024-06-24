8207ea9a2f605b34f85b67e45c3c069411826d9e
Seraj Omar <153351518+Seraj-Omar@users.noreply.github.com>
2024-06-24 13:01:09 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,52 @@
+/*
+ بسم الله الرحمن الرحيم
+
+ Code by SerajOmar :)
+
+
+     ███████ ███████ ██████   █████      ██████
+     ██      ██      ██   ██ ██   ██       ██
+     ███████ █████   ██████  ███████       ██
+          ██ ██      ██   ██ ██   ██ ██    ██
+     ███████ ███████ ██   ██ ██   ██ ████████
+                                    ⠀    -HIM-
+    LETS GOOOOO
+ */
+#include <bits/stdc++.h>
+#define SerajOmar    ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define OneTest     solve();
+#define MultipleTests     int t;cin>>t;while(t--){solve();}
+#define ll  long long
+#define vi   vector<ll>
+#define pi  pair<ll,ll>
+#define vp  vector<pi>
+#define yes  cout<<"YES"<<endl;
+#define no  cout<<"NO"<<endl;
+#define input(n)    for(auto& i:n)cin>>i;
+#define loop(from,to)   for(int i=from;i<to;i++)
+#define loop2(from2,to2)   for(int j=from2;j<to2;j++)
+#define all(v)  v.begin(),v.end()
+#define STFU    return;
+using namespace std;
+void solve()
+{
+    ll a,b;cin >> a >> b;
+    if (b > a)
+        cout << b - a << endl;
+
+    else if (a % b == 0)
+        cout << "0" << endl;
+
+    else if (a > b)
+        cout << ((a/b+1) * b)-a << endl;
+    STFU
+}
+int main()
+{
+    SerajOmar
+    MultipleTests
+}
+/*
+  CONSISTENCY.
+  Nice To Meet U ^_^
+*/
