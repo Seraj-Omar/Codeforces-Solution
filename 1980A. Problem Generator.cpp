91fe9738fc0862c0c323dc2338906eb29bbdfd91
Seraj Omar <153351518+Seraj-Omar@users.noreply.github.com>
2024-06-24 11:01:24 +0300

    Add files via upload

---
+++ 1980A. Problem Generator.cpp
@@ -0,0 +1,58 @@
+/*
+ بسم الله الرحمن الرحيم
+
+ Code by SerajOmar :)
+
+
+     ███████ ███████ ██████   █████      ██████
+     ██      ██      ██   ██ ██   ██       ██
+     ███████ █████   ██████  ███████       ██
+          ██ ██      ██   ██ ██   ██ ██    ██
+     ███████ ███████ ██   ██ ██   ██ ████████
+                                    ⠀    -HIM-
+    LETS GOOOOO
+ */
+#include <bits/stdc++.h>
+#define SerajOmar    ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define OneTest     solve();
+#define MultipleTests     int t;cin>>t;while(t--){solve();}
+#define ll  long long
+#define vi   vector<ll>
+#define pi  pair<ll,ll>
+#define vp  vector<pi>
+#define cy  cout<<"YES"<<endl;
+#define cn  cout<<"NO"<<endl;
+#define input(n)    for(auto& i:n)cin>>i;
+#define loop(from,to)   for(int i=from;i<to;i++)
+#define loop2(from2,to2)   for(int j=from2;j<to2;j++)
+#define all(v)  v.begin(),v.end()
+#define STFU    return;
+using namespace std;
+void solve()
+{
+
+    ll n,m;cin>>n>>m;
+    string s;cin>>s;
+
+    map<char, ll> map;
+    for (char i : s)
+        map[i]++;
+
+    ll counter = 0;
+    for (char c = 'A'; c <= 'G'; ++c)
+    {
+        if (map[c] < m)
+            counter += (m - map[c]);
+    }
+    cout << counter << endl;
+    STFU
+}
+int main()
+{
+    SerajOmar
+    MultipleTests
+}
+/*
+  CONSISTENCY.
+  Nice To Meet U ^_^
+*/
