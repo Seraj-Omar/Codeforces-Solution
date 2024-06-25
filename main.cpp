f5da10a20b005c1272cb6b8d3d2c0f16ae00c87f
Seraj Omar <153351518+Seraj-Omar@users.noreply.github.com>
2024-06-25 12:37:14 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,49 @@
+/*
+ بسم الله الرحمن الرحيم
+
+ Code by SerajOmar :)
+
+
+     ███████ ███████ ██████   █████      ██████
+     ██      ██      ██   ██ ██   ██       ██
+     ███████ █████   ██████  ███████       ██
+          ██ ██      ██   ██ ██   ██ ██    ██
+     ███████ ███████ ██   ██ ██   ██ ████████
+                                    ⠀    -HIM-
+    LETS GOOOOO
+ */
+#include <bits/stdc++.h>
+#define SerajOmar    ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define OneTest     solve();
+#define MultipleTests     int t;cin>>t;while(t--){solve();}
+#define ll  long long
+#define vi   vector<ll>
+#define vt(v,n,m)   vector<vector<int>> v(n, vector<int>(m));
+#define pi  pair<ll,ll>
+#define vp  vector<pi>
+#define yes  cout<<"YES"<<endl;
+#define no  cout<<"NO"<<endl;
+#define input(n)    for(auto& i:n)cin>>i;
+#define loop(from,to)   for(int i=from;i<to;i++)
+#define loop2(from2,to2)   for(int j=from2;j<to2;j++)
+#define all(v)  v.begin(),v.end()
+#define STFU    return;
+using namespace std;
+void solve()
+{
+    string a,b;
+    cin>>a>>b;
+
+    swap(a[0],b[0]);
+    cout<<a<<" "<<b<<endl;
+    STFU
+}
+int main()
+{
+    SerajOmar
+    MultipleTests
+}
+/*
+  CONSISTENCY.
+  Nice To Meet U ^_^
+*/
