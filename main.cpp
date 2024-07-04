58a3df2d459be7fea0776dd2fd1f2f898356023e
Seraj Omar <153351518+Seraj-Omar@users.noreply.github.com>
2024-07-04 10:52:34 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,63 @@
+/*
+ بسم الله الرحمن الرحيم
+
+ Code by SerajOmar :)
+
+
+     ███████ ███████ ██████   █████      ██████
+     ██      ██      ██   ██ ██   ██       ██
+     ███████ █████   ██████  ███████       ██
+          ██ ██      ██   ██ ██   ██ ██    ██
+     ███████ ███████ ██   ██ ██   ██ ████████
+                                    ⠀    -HIM-
+    LETS GOOOOO
+ */
+#include <bits/stdc++.h>
+#define SerajOmar    ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define ll  long long
+#define vi(ll)   vector<ll>
+#define vv(ll)  vector<vector<ll>>
+#define vt(v,n,m,int)   vector<vector<int>> v(n, vector<int>(m));
+#define pi  pair<ll,ll>
+#define vp  vector<pi>
+#define yes  cout<<"YES"<<endl;
+#define no  cout<<"NO"<<endl;
+#define input(n)    for(auto& i:n)cin>>i;
+#define loop(from,to)   for(int i=from;i<to;i++)
+#define loopr(from,to)   for(int i=from;i>=to;--i)
+#define loop2(from2,to2)   for(int j=from2;j<to2;j++)
+#define all(v)  v.begin(),v.end()
+#define is(x) cout<<((x)?"YES\n":"NO\n");
+#define SEEYAH    return;
+using namespace std;
+void solve()
+{
+    ll n, m;cin >> n >> m;
+    vi(ll) v(n+1,0);
+
+    ll a,b;
+    loop(0,m)
+    {
+        cin>>a>>b;
+        v[a]++;v[b]++;
+    }
+    ll counter = -1;
+    loop(1, n + 1)
+    {
+        if (v[i] > 1)
+            counter++;
+    }
+    cout << counter << " " <<  (n - counter - 1) / counter << endl;
+    SEEYAH
+}
+int main()
+{
+    SerajOmar
+    ll t=1;
+    cin>>t;
+    while(t--){solve();}
+}
+/*
+  CONSISTENCY.
+  Nice To Meet U ^_^
+*/
