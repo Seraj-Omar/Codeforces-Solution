7a6b0817524656b1815a705da1e589f74415a74f
Seraj Omar <153351518+Seraj-Omar@users.noreply.github.com>
2024-07-04 10:43:24 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,74 @@
+/*
+ بسم الله الرحمن الرحيم
+
+ Code by SerajOmar :)
+
+
+     ███████ ███████ ██████   █████      ██████
+     ██      ██      ██   ██ ██   ██       ██
+     ███████ █████   ██████  ███████       ██
+          ██ ██      ██   ██ ██   ██ ██    ██
+     ███████ ███████ ██   ██ ██   ██ ████████
+                                    ⠀    -HIM-
+    LETS GOOOOO
+ */
+#include <bits/stdc++.h>
+#define SerajOmar    ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define ll  long long
+#define vi(ll)   vector<ll>
+#define vv(ll)  vector<vector<ll>>
+#define vt(v,n,m,int)   vector<vector<int>> v(n, vector<int>(m));
+#define pi  pair<ll,ll>
+#define vp  vector<pi>
+#define yes  cout<<"YES"<<endl;
+#define no  cout<<"NO"<<endl;
+#define input(n)    for(auto& i:n)cin>>i;
+#define loop(from,to)   for(int i=from;i<to;i++)
+#define loopr(from,to)   for(int i=from;i>=to;--i)
+#define loop2(from2,to2)   for(int j=from2;j<to2;j++)
+#define all(v)  v.begin(),v.end()
+#define is(x) cout<<((x)?"YES\n":"NO\n");
+#define SEEYAH    return;
+using namespace std;
+void solve()
+{
+    ll n;cin>>n;
+
+    ll time;string s;
+    ll both=INT_MAX,first=INT_MAX,second=INT_MAX;
+    bool can=false,f=false,se=false;
+    loop(0,n)
+    {
+        cin>>time>>s;
+        if(s=="11") {
+            both = min(time, both);
+            can=true;
+        }
+
+        else if(s=="10") {
+            first = min(time, first);
+            f=true;
+        }
+
+        else if(s=="01") {
+            second = min(time, second);
+            se= true;
+        }
+    }
+    if((se&&f)||can)
+        cout<<min(both,first+second)<<endl;
+    else
+        cout<<-1<<endl;
+    SEEYAH
+}
+int main()
+{
+    SerajOmar
+    ll t=1;
+    cin>>t;
+    while(t--){solve();}
+}
+/*
+  CONSISTENCY.
+  Nice To Meet U ^_^
+*/
