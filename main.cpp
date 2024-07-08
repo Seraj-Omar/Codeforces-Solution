d946d7935e7bb5d1414092375dde8f09f3a9c91d
Seraj Omar <153351518+Seraj-Omar@users.noreply.github.com>
2024-07-08 11:54:40 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,63 @@
+/*
+ بسم الله الرحمن الرحيم
+
+ Code by SerajOmar :)
+
+
+     ███████ ███████ ██████   █████      ██████
+     ██      ██      ██   ██ ██   ██       ██
+     ███████ █████   ██████  ███████       ██
+          ██ ██      ██   ██ ██   ██ ██    ██
+     ███████ ███████ ██   ██ ██   ██ ████████
+                                        -BATMAN-
+
+    i am the night...i am vengeance...I AM BATMAN
+
+    LETS GOOOOO
+ */
+#include <bits/stdc++.h>
+#define SerajOmar    ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define ll  long long
+#define ve(ll)   vector<ll>
+#define vv(ll)  vector<vector<ll>>
+#define vt(v,n,m,int)   vector<vector<int>> v(n, vector<int>(m));
+#define pi  pair<ll,ll>
+#define vp  vector<pi>
+#define yes  cout<<"YES\n";
+#define no  cout<<"NO\n";
+#define input(n)    for(auto& i:n)cin>>i;
+#define loop(from,to)   for(int i=from;i<to;i++)
+#define loopr(from,to)   for(int i=from;i>=to;--i)
+#define loop2(from2,to2)   for(int j=from2;j<to2;j++)
+#define loop3(from3,to3)   for(int k=from3;k<to3;k++)
+#define all(v)  v.begin(),v.end()
+#define is(x) cout<<((x)?"YES\n":"NO\n");
+#define endl    cout<<'\n'
+#define SEEYAH    return;
+using namespace std;
+void solve()
+{
+    ve(ll)v(4);
+    input(v)
+
+    string s;cin>>s;
+
+    ll calories=0;
+    loop(0,s.size())
+    {
+        calories+=v[s[i]-'0'-1];
+    }
+    cout<<calories<<'\n';
+    SEEYAH
+}
+int main()
+{
+    SerajOmar
+    ll t=1;
+//    cin>>t;
+    while(t--){solve();}
+}
+/*
+  CONSISTENCY.
+  Nice To Meet U ^_^
+*/
