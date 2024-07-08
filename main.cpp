bb648d505c269962c0b03773e969d3bc128db9ae
Seraj Omar <153351518+Seraj-Omar@users.noreply.github.com>
2024-07-08 11:23:26 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,88 @@
+/*
+ بسم الله الرحمن الرحيم
+
+ Code by SerajOmar :)
+
+
+     ███████ ███████ ██████   █████      ██████
+     ██      ██      ██   ██ ██   ██       ██
+     ███████ █████   ██████  ███████       ██
+          ██ ██      ██   ██ ██   ██ ██    ██
+     ███████ ███████ ██   ██ ██   ██ ████████
+                                        -BATMAN-
+
+    i am the night...i am vengeance...I AM BATMAN
+
+    LETS GOOOOO
+ */
+#include <bits/stdc++.h>
+#define SerajOmar    ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define ll  long long
+#define ve(ll)   vector<ll>
+#define vv(ll)  vector<vector<ll>>
+#define vt(v,n,m,int)   vector<vector<int>> v(n, vector<int>(m));
+#define pi  pair<ll,ll>
+#define vp  vector<pi>
+#define yes  cout<<"YES\n";
+#define no  cout<<"NO\n";
+#define input(n)    for(auto& i:n)cin>>i;
+#define loop(from,to)   for(int i=from;i<to;i++)
+#define loopr(from,to)   for(int i=from;i>=to;--i)
+#define loop2(from2,to2)   for(int j=from2;j<to2;j++)
+#define loop3(from3,to3)   for(int k=from3;k<to3;k++)
+#define all(v)  v.begin(),v.end()
+#define is(x) cout<<((x)?"YES\n":"NO\n");
+#define endl    cout<<'\n'
+#define SEEYAH    return;
+using namespace std;
+void solve()
+{
+    ll n; cin>>n;
+    deque<ll>d(n);
+    for(ll& i:d)
+        cin>>i;
+
+    ll sereja=0,dima=0;
+    loop(0,n)
+    {
+        if(i%2)
+        {
+            if(d[0]<d[d.size()-1])
+            {
+                dima+=d[d.size()-1];
+                d.pop_back();
+            }
+            else
+            {
+                dima+=d[0];
+                d.pop_front();
+            }
+        }
+        else
+        {
+            if(d[0]<d[d.size()-1])
+            {
+                sereja+=d[d.size()-1];
+                d.pop_back();
+            }
+            else
+            {
+                sereja+=d[0];
+                d.pop_front();
+            }
+        }
+    }
+    cout<<sereja<<' '<<dima<<'\n';
+    SEEYAH
+}
+int main()
+{
+    SerajOmar
+    ll t=1;
+//    cin>>t;
+    while(t--){solve();}
+}
+/*
+  CONSISTENCY.
+  Nice To Meet U ^_^
+*/
