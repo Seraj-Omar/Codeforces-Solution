2538274fe4ddaaf75efff0cc8f80633dc840fbb2
Seraj Omar <153351518+Seraj-Omar@users.noreply.github.com>
2024-07-19 10:02:30 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,76 @@
+/*
+ بسم الله الرحمن الرحيم
+
+ Code by SerajOmar :)
+
+
+     ███████ ███████ ██████   █████      ██████
+     ██      ██      ██   ██ ██   ██       ██
+     ███████ █████   ██████  ███████       ██
+          ██ ██      ██   ██ ██   ██ ██    ██
+     ███████ ███████ ██   ██ ██   ██ ████████
+                                        -BATMAN-
+
+    i am the night...i am vengeance...I AM BATMAN
+
+    LETS GOOOOO
+ */
+#include <bits/stdc++.h>
+#define SerajOmar    ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define ll  long long
+#define ve(ll)   vector<ll>
+#define vv(ll)  vector<vector<ll>>
+#define vt(v,n,m,int)   vector<vector<int>> v(n, vector<int>(m));
+#define pi(ll,int)  pair<ll,int>
+#define vp(ll,int)  vector<pi(ll,int)>
+#define pb(n)  push_back(n)
+#define yes  cout<<"YES\n";
+#define no  cout<<"NO\n";
+#define input(n)    for(auto& i:n)cin>>i;
+#define loop(from,to)   for(int i=from;i<to;i++)
+#define loopr(from,to)   for(int i=from;i>=to;--i)
+#define loop2(from2,to2)   for(int j=from2;j<to2;j++)
+#define loop3(from3,to3)   for(int k=from3;k<to3;k++)
+#define all(v)  v.begin(),v.end()
+#define is(x) cout<<((x)?"Yes\n":"No\n");
+#define endd    '\n'
+#define SEEYAH    return;
+#define BYE       return 0;
+using namespace std;
+void solve()
+{
+    ll n,k;cin>>n>>k;
+    ve(ll)v(n);
+    cin>>v[0];
+    loop(1,n) {
+        cin>>v[i];
+        v[i] += v[i - 1];
+    }
+
+    if(n==1||n==k)
+    {
+        cout<<1<<endd;
+        SEEYAH
+    }
+    ll Min=LONG_LONG_MAX,index=-1;
+    for (int i = 0; i < n-k+1; i++)
+    {
+        if(Min>v[i+k-1]-v[i-1]) {
+            Min = v[i + k - 1] - v[i - 1];
+            index = i;
+        }
+    }
+    cout<<index+1<<endd;
+    SEEYAH
+}
+int main()
+{
+    SerajOmar
+    ll t=1;
+//    cin>>t;
+    while (t--){solve();}
+}
+/*
+  CONSISTENCY.
+  Nice To Meet U ^_^
+*/
