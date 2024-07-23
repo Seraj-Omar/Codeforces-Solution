b61865e5cfda91ff31338b789644173a6d92dbb8
Seraj Omar <153351518+Seraj-Omar@users.noreply.github.com>
2024-07-23 12:33:42 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,92 @@
+/*
+ بسم الله الرحمن الرحيم
+
+ Code by SerajOmar :)
+
+
+     ███████ ███████ ██████   █████      ██████
+     ██      ██      ██   ██ ██   ██       ██
+     ███████ █████   ██████  ███████       ██
+          ██ ██      ██   ██ ██   ██ ██    ██
+     ███████ ███████ ██   ██ ██   ██ ████████
+                                        -BATMAN-
+
+    i am the night...i am vengeance...I AM BATMAN
+
+    LETS GOOOOO
+ */
+#include <bits/stdc++.h>
+#define SerajOmar    ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define ll  long long
+#define ve(ll)   vector<ll>
+#define vv(ll)  vector<vector<ll>>
+#define vt(v,n,m,int)   vector<vector<int>> v(n, vector<int>(m));
+#define vti(v,n,m,int,initial)   vector<vector<int>> v(n, vector<int>(m,initial));
+#define pi(ll,int)  pair<ll,int>
+#define vp(ll,int)  vector<pi(ll,int)>
+#define pb(n)  push_back(n)
+#define yes  cout<<"YES\n";
+#define no  cout<<"NO\n";
+#define input(n)    for(auto& i:n)cin>>i;
+#define loop(from,to)   for(int i=from;i<to;i++)
+#define loopr(from,to)   for(int i=from;i>=to;--i)
+#define loop2(from2,to2)   for(int j=from2;j<to2;j++)
+#define loop3(from3,to3)   for(int k=from3;k<to3;k++)
+#define all(v)  v.begin(),v.end()
+#define is(x) cout<<((x)?"Yes\n":"No\n");
+#define endd    '\n'
+#define SEEYAH    return;
+#define BYE       return 0;
+using namespace std;
+//vp(int,int) directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+void solve()
+{
+    ll n,m,k,t;
+    cin>>n>>m>>k>>t;
+
+    ve(ll)v;
+    map<char,string>cell{{'w',"Waste"},{'c',"Carrots"},{'k',"Kiwis"},{'g',"Grapes"}};
+
+    ll l,r;
+    loop(0,k)
+    {
+        cin>>l>>r;
+        l--;r--;
+        v.push_back(l*m+r);
+    }
+    sort(all(v));
+    set<ll>st(all(v));
+    string s="ckg";
+    while(t--)
+    {
+        ll counter=0;
+        cin>>l>>r;
+        l--;r--;
+        if(st.find(l*m+r)!=st.end())
+            cout<<"Waste"<<endd;
+
+        else
+        {
+            loop(0,v.size())
+            {
+                if(v[i]<l*m+r)
+                    counter++;
+            }
+            cout<<cell[s[(l*m+r-counter)%3]]<<endd;
+        }
+    }
+}
+int main()
+{
+    SerajOmar
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    ll t=1;
+//    cin>>t;
+    while (t--){solve();}
+    BYE
+}
+/*
+  CONSISTENCY.
+  Nice To Meet U ^_^
+*/
