cf4df5de2dc358245a8be1db9cc0d3ad8363d1b1
Seraj Omar <153351518+Seraj-Omar@users.noreply.github.com>
2024-08-13 13:24:55 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+#define SerajOmar    ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define ll  long long
+#define all(x) x.begin(),x.end()
+#define allr(x) x.rbegin(),x.rend()
+#define is(x) cout<<((x)?"YES\n":"NO\n");
+#define endd    '\n'
+using namespace std;
+vector<pair<ll,ll>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+void solve()
+{
+    ll n;cin>>n;
+    vector<ll>powers,divisors;
+    for (int i = 0; i <= 32; ++i)
+        powers.push_back(pow(2,i));
+
+    for (int i = 1; i*i <=n ; ++i)
+    {
+        if(n%i==0)
+        {
+            divisors.push_back(i);
+            if (i * i != n)
+                divisors.push_back(n / i);
+        }
+    }
+    sort(allr(divisors));
+    for(auto i:divisors)
+    {
+        for (int j = 1; j < powers.size(); ++j)
+        {
+            if((powers[j]-1)*(powers[j-1])==i)
+            {
+                cout<<i<<endd;
+                return;
+            }
+        }
+    }
+}
+int main()
+{
+    SerajOmar
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+//    cout<<fixed<<setprecision(9);
+    ll t=1;
+//    cin>>t;
+    while (t--){solve();}
+    return 0;
+}
+/*
+  CONSISTENCY.
+  Nice To Meet U ^_^
+*/
+/*
+   i>=0&&j>=0&&i<n&&j<m;
+
+   (n * (n + 1)) even 1-n but you use n/2 not n (n/2*(n/2+1)
+   pow(((n + 1) / 2),2) odd 1-n
+   n(n+1)/2 sum 1-n
+
+   (a+b)%c = ((a%c)+(b%c))%c
+   (a-b)%c = ((a%c)-(b%c)+c)%c
+   (a*b)%c = ((a%c)*(b%c))%c
+   (a/b)%c = ((a%c)*(pow(b,-1)%c)%c
+
+   Lowercase 97-122
+   Uppercase 65-90
+
+   Do you remember the complexity table?
+   Some estimations that may help
+
+    N                      complexity                  Possible Algorithms & Techniques
+
+    10^18                  O(log(N))                   Binary & Ternary Search / Matrix Power / Cycle Tricks / Big Simulation Steps / Values ReRank
+    100 000 000            O(N)                        A Linear Solution - May be a greedy algorithm
+    40 000 000             O(N log N)                  Linear # calls to Binary & Ternary Search / Pre-processing & Querying / D & C
+    10 000                 O(N2)                       adhock / DP / Greedy / D & C / B & B
+    500                    O(N3)                       adhock / DP / Greedy / ..
+    90                     O(N4)                       adhock / DP / Greedy / ..
+    30-50                  -                           Search with pruning - branch and bound
+    40                     O(2^N/2)                    Meet in Middle
+    20                     O(2^N)                      Backtracking / Generating 2^N Subsets
+    11                     O(N!)                       Factorial / Permutations / Combination Algorithm
+*/
