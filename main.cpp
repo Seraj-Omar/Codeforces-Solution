971e79d7c7582bc659f23fd2c274767c48773d7d
Seraj Omar <153351518+Seraj-Omar@users.noreply.github.com>
2024-09-02 11:21:39 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define SerajOmar    ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define ll  long long
+#define is(x) cout<<((x)?"YES\n":"NO\n");
+using namespace std;
+
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+
+template<typename T>
+using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;//ordered set
+
+template<typename T>
+using o_mset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;//ordered multiset
+
+//i>=0&&j>=0&&i<n&&j<m; valid
+vector<pair<ll,ll>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+const ll mod=1e9+7;
+
+void solve()
+{
+    ll n;cin>>n;
+    vector<ll>v(n);
+    for(auto& i:v)
+        cin>>i;
+    std::sort(v.begin(), v.end());
+    v[0]++;
+
+    ll prod=1;
+    for(auto i:v)
+        prod*=i;
+    cout<<prod<<'\n';
+}
+int main()
+{
+    SerajOmar
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+//    cout<<fixed<<setprecision(9);
+
+    ll t=1;
+    cin>>t;
+    while (t--){solve();}
+    return 0;
+}
