ec1599cd9fa5359a0b05ee80724af575f4616e9b
Seraj Omar <153351518+Seraj-Omar@users.noreply.github.com>
2024-09-04 22:38:07 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#define SerajOmar    ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define ll  long long
+using namespace std;
+
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+
+template<typename T>
+using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;//ordered set
+
+template<typename T>
+using o_mset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;//ordered multiset
+
+//i>=0&&j>=0&&i<n&&j<m; valid
+vector<pair<ll,ll>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+const ll mod=1e9+7;
+void solve()
+{
+    ll n;cin>>n;
+    vector<ll>v(n);
+    for(auto& i:v)cin>>i;
+
+    std::sort(v.begin(), v.end());
+
+    ll val=v[0]*v[n-1];
+
+    vector<ll>divs;
+    for (ll i = 2; i*i <=val ; ++i)
+    {
+        if (!(val % i))
+        {
+            divs.push_back(i);
+            if (i != val / i)
+                divs.push_back(val / i);
+        }
+    }
+    std::sort(divs.begin(), divs.end());
+    if(divs.size()!=n)
+        cout<<-1<<'\n';
+    else
+    {
+        for (int i = 0; i < n; ++i) {
+            if(v[i]!=divs[i])
+            {
+                cout<<-1<<'\n';
+                return;
+            }
+        }
+        cout<<val<<'\n';
+    }
+}
+int main()
+{
+    SerajOmar
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+//    cout<<fixed<<setprecision(9);
+
+    ll t=1;
+    cin>>t;
+    while (t--){solve();}
+    return 0;
+}
