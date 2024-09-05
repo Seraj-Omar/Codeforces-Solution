d793ad62b1238c40d6ee4778e2ce4e5ef31a7ea9
Seraj Omar <153351518+Seraj-Omar@users.noreply.github.com>
2024-09-05 19:39:13 +0300

    Update 520B - Two Buttons.cpp

--- 520B - Two Buttons.cpp
+++ 520B - Two Buttons.cpp
@@ -1,50 +1,32 @@
-/*
- بسم الله الرحمن الرحيم
-
- Code by SerajOmar :)
-
-
-     ███████ ███████ ██████   █████      ██████
-     ██      ██      ██   ██ ██   ██       ██
-     ███████ █████   ██████  ███████       ██
-          ██ ██      ██   ██ ██   ██ ██    ██
-     ███████ ███████ ██   ██ ██   ██ ████████
-                                        -BATMAN-
-
-    i am the night...i am vengeance...I AM BATMAN
-
-    LETS GOOOOO
- */
 #include <bits/stdc++.h>
 #define SerajOmar    ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
 #define ll  long long
-#define ve(ll)   vector<ll>
-#define vv(ll)  vector<vector<ll>>
-#define vt(v,n,m,int)   vector<vector<int>> v(n, vector<int>(m));
-#define pi(ll,int)  pair<ll,int>
-#define vp(ll,int)  vector<pi(ll,int)>
-#define pb(n)  push_back(n)
-#define yes  cout<<"YES\n";
-#define no  cout<<"NO\n";
-#define input(n)    for(auto& i:n)cin>>i;
-#define loop(from,to)   for(int i=from;i<to;i++)
-#define loopr(from,to)   for(int i=from;i>=to;--i)
-#define loop2(from2,to2)   for(int j=from2;j<to2;j++)
-#define loop3(from3,to3)   for(int k=from3;k<to3;k++)
-#define all(v)  v.begin(),v.end()
-#define is(x) cout<<((x)?"Yes\n":"No\n");
-#define endd    '\n'
-#define SEEYAH    return;
-#define BYE       return 0;
 using namespace std;
-void solve() {
+
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+
+template<typename T>
+using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;//ordered set
+
+template<typename T>
+using o_mset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;//ordered multiset
+
+//i>=0&&j>=0&&i<n&&j<m; valid
+vector<pair<ll,ll>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+const ll mod=1e9+7;
+
+void solve()
+{
     ll n, m;
     cin >> n >> m;
 
     queue<ll> q;
     q.push(n);
-    ve(ll) value(1e5);
-    ve(bool) vis(1e5);
+
+    vector<ll> value(1e5);
+    vector<bool> vis(1e5);
 
     while (!q.empty())
     {
@@ -53,10 +35,9 @@ void solve() {
 
         if (x == m)
         {
-            cout << y << endd;
-            SEEYAH
+            cout << y << '\n';
+            return;
         }
-
         q.pop();
 
         if (x < m && !vis[x * 2])
@@ -73,17 +54,16 @@ void solve() {
             q.push(x - 1);
         }
     }
-
-    SEEYAH
 }
 int main()
 {
     SerajOmar
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+//    cout<<fixed<<setprecision(9);
+
     ll t=1;
 //    cin>>t;
     while (t--){solve();}
+    return 0;
 }
-/*
-  CONSISTENCY.
-  Nice To Meet U ^_^
-*/
