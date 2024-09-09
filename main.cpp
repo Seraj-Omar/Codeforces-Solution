63b47e76594c168b9823aecd139756652dd4228c
Seraj Omar <153351518+Seraj-Omar@users.noreply.github.com>
2024-09-09 10:46:46 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#define SerajOmar    ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define ll  long long
+using namespace std;
+
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+
+template<typename T>
+using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;//ordered set
+
+template<typename T>
+using o_mset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;//ordered multiset
+
+//i>=0&&j>=0&&i<n&&j<m; valid
+//vector v(n,vector(m,vector<ll>(k)));//3D vector
+//vector<vector<ll>> vec(n,vector<int>(m))//2D vector
+vector<pair<ll,ll>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+const ll mod=1e9+7;
+
+ll n;
+map<ll,vector<ll>>v;
+void dfs(ll i,ll p)
+{
+    cout<<i<<' ';
+    for(auto j:v[i])
+    {
+        if(j!=p)
+            dfs(j,i);
+    }
+}
+void solve()
+{
+    cin>>n;
+    set<ll>s;
+    for (int i = 0; i < n; ++i)
+    {
+        ll x,y;cin>>x>>y;
+        v[x].push_back(y);
+        v[y].push_back(x);
+        s.insert(x);
+        s.insert(y);
+    }
+    for(auto i:s)
+    {
+        if(v[i].size()==1)
+        {
+            dfs(i,-1);
+            return;
+        }
+    }
+}
+int main()
+{
+    SerajOmar
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+//    cout<<fixed<<setprecision(9);
+    ll t=1;
+//    cin>>t;
+    while (t--){solve();}
+    return 0;
+}
