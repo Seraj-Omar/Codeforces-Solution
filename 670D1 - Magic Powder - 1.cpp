599c2af9420a336668b8961b93026e50b5738968
Seraj Omar <153351518+Seraj-Omar@users.noreply.github.com>
2024-09-21 16:57:19 +0300

    Update 670D1 - Magic Powder - 1.cpp

--- 670D1 - Magic Powder - 1.cpp
+++ 670D1 - Magic Powder - 1.cpp
@@ -1,51 +1,51 @@
 #include <bits/stdc++.h>
 #define SerajOmar    ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
-#define ll  long long
-#define loop(from,to)   for(int i=from;i<to;i++)
-#define loopr(from,to)   for(int i=from;i>=to;--i)
-#define loop2(from,to)   for(int j=from;j<to;j++)
-#define loop3(from,to)   for(int k=from;k<to;k++)
-#define all(x) x.begin(),x.end()
-#define allr(x) x.rbegin(),x.rend()
-#define is(x) cout<<((x)?"YES\n":"NO\n");
+#define ll           long long int
+#define ld           long double
 using namespace std;
 
-//#include <ext/pb_ds/tree_policy.hpp>
-//#include <ext/pb_ds/assoc_container.hpp>
-//using namespace __gnu_pbds;
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
 
-//template<typename T>
-//using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;//ordered set
+template<typename T>
+using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;//ordered set
 
-//template<typename T>
-//using o_mset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;//ordered multiset
+template<typename T>
+using o_mset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;//ordered multiset
 
-vector<pair<ll,ll>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
-const ll mod=1e9+7;
 //i>=0&&j>=0&&i<n&&j<m; valid
-// can be used for (LCM , GCD , MIN , MAX , | , &)
+//vector v(n,vector(m,vector<ll>(k)));//3D vector
+//vector<vector<ll>> v(n,vector<int>(m))//2D vector
+vector<pair<ll,ll>> di = {{1,1},{-1,-1},{0,0}};
+const ll mod=1e9+7;
+const ll inf=2e18+1;
+
 ll n,k;
-vector<ll>a;
-vector<ll>b;
+vector<ll>a,b;
 bool valid(ll mid)
 {
     ll sum=0;
-    loop(0,n)
+    for (int i = 0; i < n; ++i)
     {
         if(mid * a[i] > b[i])
-            sum+=(mid * a[i] - b[i]);
+            sum+=((mid * a[i]<=b[i])?0:mid*a[i] - b[i]);
+        if(sum>k)
+            return false;
     }
-    return sum<=k;
+    return true;
 }
 void solve()
 {
     cin>>n>>k;
     a.resize(n);
     b.resize(n);
-    for(auto& i:a)cin>>i;
-    for(auto& i:b)cin>>i;
+    for(auto& i:a)
+        cin>>i;
+    for(auto& i:b)
+        cin>>i;
 
-    ll l=0,r=1e6,mid;
+    ll l=0,r=1e10,mid;
     while(r-l>1)
     {
         mid=(r+l)/2;
@@ -61,7 +61,7 @@ int main()
     SerajOmar
 //    freopen("input.txt", "r", stdin);
 //    freopen("output.txt", "w", stdout);
-//    cout<<fixed<<setprecision(9);
+//    cout<<fixed<<setprecision(10);
     ll t=1;
 //    cin>>t;
     while (t--){solve();}
